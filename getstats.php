275a2d20467ef8df3f0eb907d977fbfb28a63325
root <root@r.sehoco.de>
2016-12-08 20:21:10 +0100

    hero config outsourced

--- getstats.php
+++ getstats.php
@@ -16,56 +16,59 @@ function get_stats($battletag,$function="general",$apiv="v2")
 	return json_decode(api_request($battletag,$function));
 }
 
-$heroes = array(
-	'offense' => array(
-	  "mccree",
-          "genji",
-          "reaper",
-          "tracer",
-          "pharah",
-          "soldier76",
-          "sombra"
-	),
-	'defense' => array(
-          "bastion",
-          "widowmaker",
-          "torbjorn",
-          "junkrat",
-          "mei",
-          "hanzo"
-	),
-	'tank' => array(
-          "reinhardt",
-          "zarya",
-          "winston",
-          "roadhog",
-          "dva"
-	),
-	'support' => array(
-          "zenyatta",
-          "symmetra",
-          "ana",
-          "lucio",
-          "mercy"
-	)
-);
-
-$blocker = array(
-        "mei", "reinhardt", "zarya", "winston", "dva"
-);
 
 foreach ($player as $tag)
 {
+  foreach (array("QM","COMPETITIVE") as $mode)
+  {
+	$modestr = ($mode=="QM" ? "general" : "competitive");
 	echo $tag;
 	$a = explode("#", $tag);
-	$ob = get_stats($tag,"stats/general");
+	$ob = get_stats($tag,"stats/$modestr");
+	$ob_herolist = get_stats($tag,"heroes/$modestr");
+	$ob_herolist = $ob_herolist->heroes;
 	$games_played = round($ob->game_stats->damage_done / $ob->average_stats->damage_done_avg);
 	$dmg_blocked=0;
-	foreach ($blocker as $hero)
+	foreach ($heroes as $class)
 	{
-		$ob_hero = get_stats($tag,"heroes/".$hero);
+	 foreach ($class as $hero)
+	 {
+	  echo ".";
+	  if (in_array($hero,$blocker))
+	  {
+		if (!$ob_herolist->$hero) continue;
+		$ob_hero = get_stats($tag,"heroes/$hero/$modestr");
+		/*
+		$db->query(
+		"insert into ow_heroes
+			(`tag`, 
+			`mode`, 
+			`date`, 
+			`hero`,
+			`wins`, 
+			`games`, 
+			`kills`, 
+			`deaths`, 
+			`damage`, 
+			`blocked`, 
+			`healing`) 
+		values (
+			'$tag',
+			'QM',
+			'".date("Y-m-d")."',
+			'".$hero."',
+			".$ob_hero->game_stats->games_won.",
+			".$games_played.",
+			".$ob->game_stats->eliminations.",
+			".$ob->game_stats->deaths.",
+			".$ob->game_stats->damage_done.",
+			".$dmg_blocked.",
+			".$ob->game_stats->healing_done."
+		)");
+		*/
 		$dmg_blocked += $ob_hero->hero_stats->damage_blocked;
-		echo ".";
+	  }
+	 }
 	}
 	$db->query(
 	"insert into ow_general 
@@ -81,7 +84,7 @@ foreach ($player as $tag)
 		`healing`) 
 	values (
 		'$tag',
-		'QM',
+		'$mode',
 		'".date("Y-m-d")."',
 		".$ob->game_stats->games_won.",
 		".$games_played.",
@@ -92,5 +95,6 @@ foreach ($player as $tag)
 		".$ob->game_stats->healing_done."
 	)");
 	echo "\n";
+  }
 }
 
