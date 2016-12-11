a6c13e7382c73068c4adc1318a30da06a94f1723
Jochen Pylypiw <jochen@pylypiw.com>
2016-12-11 22:15:47 +0100

    changes and development

--- statsout.php
+++ statsout.php
@@ -3,6 +3,8 @@ require "config.inc.php";
 require "db.php";
 
 define('DELIM',',');
+define('DELIMD','|');
+$comparedays = 7;
 
 $mode = $argv[1];
 if (!$mode) $mode = "QM";
@@ -41,24 +43,40 @@ $q = $db->query("select * from ow_general where `mode`='$mode' and `date`='$rece
 while ($r = $q->fetch_object()) 
 {
 	$tag = $r->tag;
+	$ql = $db->query("
+		select * from ow_general where `mode`='$mode' and `date` <= ('$recentdate' - INTERVAL $comparedays day) and `tag`='$tag' ORDER BY `date` desc");
+	$rl = $ql->fetch_object();
+	if (!$rl) {
+		$ql = $db->query("
+		select * from ow_general where `mode`='$mode' and `date`>('$recentdate' - INTERVAL $comparedays day) and `tag`='$tag' ORDER BY `date` "); 
+		$rl = $ql->fetch_object();
+	}
+	$datediff = (strtotime($rl->date) - strtotime($r->date))/60/60/24;
 	$a = explode("#", $tag);
-	echo $a[0].DELIM;
-	$games_played = $r->games;
-	echo $r->rating.DELIM;
-	echo $games_played.DELIM;
-	echo $r->wins.DELIM;
-	if ($games_played>0)
+	echo $a[0]." (${datediff}d)".DELIM;
+	echo $r->rating.DELIMD;
+	echo ($r->rating-$rl->rating).DELIM;
+	echo $r->games.DELIMD;
+	echo ($r->games-$rl->games).DELIM;
+	echo $r->wins.DELIMD;
+	echo ($r->wins-$rl->wins).DELIM;
+	if ($r->games>0)
 	{
-		echo ($r->wins / $games_played)*100 . DELIM;
+		echo ($r->wins / $r->games)*100 . DELIMD;
+		echo (($r->wins / $r->games)*100 - ($rl->wins / $rl->games)*100 ). DELIM;
 	} else {
 		echo "0".DELIM;
 	}
-	echo $r->kills/$games_played . DELIM;
-	echo number_format($r->kills/$r->deaths,5,".","").DELIM;
-	echo $r->damage/$games_played.DELIM;
-	$dmg_blocked=$r->blocked;
-	echo number_format($dmg_blocked / $games_played,5,".","").DELIM;
-	echo $r->healing / $games_played;
+	echo $r->kills/$r->games . DELIMD;
+	echo ($r->kills/$r->games - $rl->kills/$rl->games ) . DELIM;
+	echo $r->kills/$r->deaths.DELIMD;
+	echo ($r->kills/$r->deaths - $rl->kills/$rl->deaths ).DELIM;
+	echo $r->damage/$r->games.DELIMD;
+	echo ($r->damage/$r->games - $rl->damage/$rl->games).DELIM;
+	echo $r->blocked / $r->games.DELIMD;
+	echo ($r->blocked / $r->games - $rl->blocked / $rl->games).DELIM;
+	echo $r->healing / $r->games.DELIMD;
+	echo ($r->healing / $r->games - $rl->healing / $rl->games);
 	echo "\n";
 }
 
