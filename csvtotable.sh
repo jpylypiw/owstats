7ffe17b8e59f38407e8937b1d94143070ac45381
Roman Krewer <roman.krewer@sehoco.de>
2016-12-14 12:32:05 +0100

    generation time

--- csvtotable.sh
+++ csvtotable.sh
@@ -4,4 +4,4 @@ OUTFILE=index.html
 
 cat index.header > $OUTFILE
 for csv in stats_*.csv; do php csvtotable.php $csv >> $OUTFILE; done
-cat index.footer >> $OUTFILE
+cat index.footer | sed "s/GENTIMESTRING/$(date)/" >> $OUTFILE
