3489a270a28d5766399a57134b20c7712fd30389
Roman Krewer <roman.krewer@sehoco.de>
2017-02-20 17:57:39 +0100

    added S3/Q3 MISC/HIGH to DB and GetStats. Put api function to function.php

--- functions.php
+++ functions.php
@@ -31,4 +31,24 @@ function histkddiffavg(&$history)
         }
         return substr($col,0,-1).DELIM;
 }
+function api_request($battletag,$function="general",$apiv="v2")
+{
+        $tag = urlencode(str_replace("#","-",$battletag));
+        $url = "http://localhost:4444/api/$apiv/u/$tag/$function";
+        $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+        $ch=curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+        $json=curl_exec($ch);
+        curl_close($ch);
+        sleep(2);
+        return $json;
+}
+
+function get_stats($battletag,$function="general",$apiv="v3")
+{
+        return json_decode(api_request($battletag,$function,$apiv));
+}
 
