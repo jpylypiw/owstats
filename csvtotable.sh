ad5cfe228ca4480f9ebb1e79af9f28203261d2b4
J. Pylypiw <jochen@pylypiw.com>
2017-09-23 20:13:13 +0200

    Branch Cleanup

--- csvtotable.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-OUTFILE=web/index.html
-
-cat index.header > $OUTFILE
-for csv in stats_*.csv; do php csvtotable.php $csv >> $OUTFILE; done
-cat index.footer | sed "s/GENTIMESTRING/$(date)/" >> $OUTFILE
