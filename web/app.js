83e8f8aa6a0567eccb95f8529b71444b64cf9d49
Jochen Pylypiw <jochen@pylypiw.com>
2017-09-26 23:54:57 +0200

    Frontend Errors and Includes

--- web/app.js
+++ web/app.js
@@ -6,8 +6,8 @@ var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var lessMiddleware = require("less-middleware");
 
-var index = require("./routes/index");
-var users = require("./routes/users");
+var comp_general = require("./routes/comp_general");
+// var users = require("./routes/users");
 
 var app = express();
 
@@ -15,8 +15,6 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,8 +22,8 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", index);
-app.use("/users", users);
+app.use("/", comp_general);
+// app.use("/users", users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,13 +34,49 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-    // render the error page
     res.status(err.status || 500);
-    res.render("error");
+    var params = {};
+
+    switch (err.status) {
+        case 400:
+            params.textColor = "text-primary";
+            params.textAnimation = "bounceInDown";
+            params.errorCode = 400;
+            params.errorText = "We are sorry but your request contains bad syntax and cannot be fulfilled..";
+            break;
+        case 401:
+            params.textColor = "text-amethyst";
+            params.textAnimation = "fadeInDown";
+            params.errorCode = 401;
+            params.errorText = "We are sorry but you are not authorized to access this page..";
+            break;
+        case 403:
+            params.textColor = "text-flat";
+            params.textAnimation = "bounceIn";
+            params.errorCode = 403;
+            params.errorText = "We are sorry but you do not have permission to access this page..";
+            break;
+        case 404:
+            params.textColor = "text-city";
+            params.textAnimation = "flipInX";
+            params.errorCode = 404;
+            params.errorText = "We are sorry but the page you are looking for was not found..";
+            break;
+        case 503:
+            params.textColor = "text-smooth";
+            params.textAnimation = "rollIn";
+            params.errorCode = 503;
+            params.errorText = "We are sorry but our service is currently not available..";
+            break;
+        default:
+            params.textColor = "text-modern";
+            params.textAnimation = "zoomInDown";
+            params.errorCode = 500;
+            params.errorText = "We are sorry but our server encountered an internal error..";
+            break;
+    }
+
+    res.render("pages/error", params);
 });
 
 module.exports = app;
--- web/routes/comp_general.js
+++ web/routes/comp_general.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var router = express.Router();
+var now = require("performance-now");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-    res.render("index", {
+    res.render("pages/comp_general", {
         title: "General",
         description: "There are general statistics about Competitive Overwatch Games.",
         breadcrumbTitle: "Competitive",
-        breadcrumbPage: "General"
+        breadcrumbPage: "General",
+        now: now,
+        startTime: now()
     });
 });
 
