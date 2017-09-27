7cd3d411fcd2bab1bf9f5dbd467bedf4e73a9d66
Jochen Pylypiw <jochen@pylypiw.com>
2017-09-27 14:27:06 +0200

    Sidebar Handling, Breadcrumb and Variable Handling

--- web/routes/comp_general.js
+++ web/routes/comp_general.js
@@ -2,15 +2,18 @@ var express = require("express");
 var router = express.Router();
 var now = require("performance-now");
 
-/* GET home page. */
 router.get("/", function(req, res, next) {
     res.render("pages/comp_general", {
-        title: "General",
-        description: "There are general statistics about Competitive Overwatch Games.",
-        breadcrumbTitle: "Competitive",
-        breadcrumbPage: "General",
-        now: now,
-        startTime: now()
+        breadcrumb: {
+            title: "General",
+            description: "There are general statistics about Competitive Overwatch Games.",
+            area: "Competitive",
+        },
+        footer: {
+            now: now,
+            startTime: now(),
+        },
+        url: req.originalUrl
     });
 });
 
